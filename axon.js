a43598d137ecf4b79821f398420c401df3234c12
Sam Sulaimanov <sam@bitmorse.com>
2016-04-08 13:54:57 -0700

    Update axon.js

--- axon.js
+++ axon.js
@@ -2,6 +2,17 @@
 
 	//call the UI page "home"
 	App.load('home');
-
+	
+	setInterval(function(){
+	
+		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/Accelerometer", function( data ) {
+			
+			$.("#sensordata").html(JSON.stringify(data));
+			
+		});
+		
+		
+	}, 1000);
+	
 
 })();
