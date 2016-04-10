77a6e72bb4c4091a2fdd7c4bf96c306e63df1823
Brooke Simmons <vrooje@gmail.com>
2016-04-09 18:24:55 -0700

    trying to add a new page

--- axon.js
+++ axon.js
@@ -1,18 +1,20 @@
 (function(){
 
 	//call the UI page "home"
-	App.load('home');
-	
+	//App.load('home');
+	App.load('bart-question');
+
 	setInterval(function(){
-	
-		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/Accelerometer", function( data ) {
-			
+
+		//$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/Accelerometer", function( data ) {
+		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/GPS", function( data ) {
+
 			$("#sensordata").html(JSON.stringify(data));
-			
+
 		});
-		
-		
+
+
 	}, 1000);
-	
+
 
 })();
